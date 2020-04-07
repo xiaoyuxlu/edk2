e17ac24d86227a55856e7c584bb54145840d58ae
Xiaoyu Lu <xiaoyux.lu@intel.com>
2020-04-07 14:37:34 +0800

    RustPkg: split osstr and osstring, add encoder for char16

--- RustPkg/External/efi-str/src/lib.rs
+++ RustPkg/External/efi-str/src/lib.rs
@@ -1,14 +1,17 @@
 #![no_std]
 
-pub use ucs2;
+pub mod encoder;
 
 #[macro_use]
 mod macros;
 
 mod os_str;
-
-pub use os_str::OsString;
 pub use os_str::OsStr;
+
+#[cfg(feature="string")]
+mod os_string;
+#[cfg(feature="string")]
+pub use os_string::OsString;
 
 #[cfg(test)]
 mod tests {
