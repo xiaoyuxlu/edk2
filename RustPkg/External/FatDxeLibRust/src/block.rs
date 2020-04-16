c0307fe4a6cf7820010307f1adff917c80ea75bd
Xiaoyu Lu <xiaoyux.lu@intel.com>
2020-04-16 15:56:00 +0800

    fat filesystem impl

---
+++ RustPkg/External/FatDxeLibRust/src/block.rs
@@ -0,0 +1,20 @@
+
+pub enum Error {
+    BlockIOError,
+
+    #[cfg(not(test))]
+    BlockNotSupported,
+}
+
+pub trait SectorRead {
+    /// Read a single sector (512 bytes) from the block device. `data` must be
+    /// exactly 512 bytes long.
+    fn read(&self, sector: u64, data: &mut [u8]) -> Result<(), Error>;
+}
+
+pub trait SectorWrite {
+    /// Write a single sector (512 bytes) from the block device. `data` must be
+    /// exactly 512 bytes long.
+    fn write(&self, sector: u64, data: &mut [u8]) -> Result<(), Error>;
+    fn flush(&self) -> Result<(), Error>;
+}
--- RustPkg/External/FatDxeLibRust/src/lib.rs
+++ RustPkg/External/FatDxeLibRust/src/lib.rs
@@ -1,12 +1,24 @@
 #![cfg_attr(not(test), no_std)]
 
-use r_efi::efi;
-
+#[macro_use]
 mod log;
 
+use r_efi::efi;
+
+mod block;
+mod part;
+mod fat;
+
+fn dump_disk_512(bs: &mut efi::BootServices) {
+    let _address = bs.locate_protocol as *const ();
+    log!("boot service: {:p}", _address);
+}
+
 #[export_name = "FatEntryPoint"]
-pub extern fn fat_entry_point(_h: efi::Handle, _st: *mut efi::SystemTable) -> efi::Status {
+pub extern fn fat_entry_point(_h: efi::Handle, st: *mut efi::SystemTable) -> efi::Status {
     log!("fat_entry");
+
+    dump_disk_512(unsafe{&mut *((*st).boot_services)});
     efi::Status::SUCCESS
 }
 
