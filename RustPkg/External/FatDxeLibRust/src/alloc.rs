30276b233cfafa66806a7e098ea4e68b2df2197b
Xiaoyu Lu <xiaoyux.lu@intel.com>
2020-04-17 16:27:55 +0800

    RustPkg/FatDxeLibRust: install SimpleFileSystemProtocol

--- RustPkg/External/FatDxeLibRust/src/alloc.rs
+++ RustPkg/External/FatDxeLibRust/src/alloc.rs
@@ -1,23 +1,31 @@
 
 #![allow(unused)]
 
-use r_efi::efi;
+use r_efi::efi::{self, Status};
+use core::ffi::c_void;
 
 extern "C" {
-    fn AllocatePool (Size: usize) -> *mut core::ffi::c_void;
-    fn FreePool (Buffer: *mut core::ffi::c_void);
+    fn AllocatePool (Size: usize) -> *mut c_void;
+    fn FreePool (Buffer: *mut c_void);
 }
 
-pub unsafe fn malloc<T>() -> Result<*mut T, efi::Status> {
+pub unsafe fn malloc<T>() -> Result<*mut T, Status> {
     let size = core::mem::size_of::<T>();
     let address = AllocatePool(size);
-    if address == core::ptr::null_mut::<core::ffi::c_void>(){
-        Err(efi::Status::OUT_OF_RESOURCES)
+    if address == core::ptr::null_mut::<c_void>(){
+        Err(Status::OUT_OF_RESOURCES)
     } else {
-        Ok(core::mem::transmute::<*mut core::ffi::c_void, *mut T>(address))
+        Ok(core::mem::transmute::<*mut c_void, *mut T>(address))
     }
 }
 
 pub unsafe fn free<T>(ptr: *mut T) {
-    FreePool(ptr as *mut core::ffi::c_void);
+    FreePool(ptr as *mut c_void);
+}
+
+pub unsafe fn duplicate<T>(d: &T) -> Result<*mut T, Status>
+{
+    let t = malloc::<T>()?;
+    unsafe {core::ptr::copy_nonoverlapping (d as *const T as *const c_void, t as * mut c_void, core::mem::size_of::<T>());}
+    Ok(t)
 }
