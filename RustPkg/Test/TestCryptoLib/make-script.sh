987f70f061bf9816765959de12c22a8c80c35294
Xiaoyu Lu <xiaoyux.lu@intel.com>
2020-05-12 18:42:41 +0800

    RustPkg/Test: use cert to verify signature
    Signed-off-by: Xiaoyu Lu <xiaoyux.lu@intel.com>

--- RustPkg/Test/TestCryptoLib/make-script.sh
+++ RustPkg/Test/TestCryptoLib/make-script.sh
@@ -10,6 +10,14 @@ popd
 mkdir -p test/ecdsa
 pushd test/ecdsa
 openssl genpkey -algorithm ec -pkeyopt ec_paramgen_curve:P-256 -pkeyopt ec_param_enc:named_curve -outform DER > private.der
-openssl.exe pkcs8 -in private.der -inform DER -topk8 -nocrypt -outform DER > private.p8
-openssl.exe ec -pubout -inform der -in private.der -outform der -out public.der
+openssl pkcs8 -in private.der -inform DER -topk8 -nocrypt -outform DER > private.p8
+openssl ec -pubout -inform der -in private.der -outform der -out public.der
+popd
+
+
+mkdir -p test/pki/rsa
+pushd test/pki/rsa
+openssl req -nodes -x509 -days 3650 -newkey rsa:4096 -keyout ca.key -out ca.cert -sha256 -batch -subj "//CN=intel RSA CA"
+openssl rsa -in ca.key -outform der -out ca.key.der
+openssl asn1parse -in ca.cert -out ca.der
 popd
